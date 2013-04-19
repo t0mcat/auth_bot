1ed8f66228492f22830255f4a6417db80e0c3c1c
t0mcat <diminoten@snoonet.org>
2013-04-19 01:17:52 -0400

    Project setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name= "auth_bot",
+    version = "0.0.1",
+    packages = ['snoonet'],
+    install_requires = ['twisted'],
+    author = "t0mcat",
+    author_email = "diminoten@snoonet.org",
+    url = "https://github.com/t0mcat/auth_bot",
+    entry_points = {
+            'console_scripts':[
+                'auth_bot = snoonet.auth:start_auth_bot'
+            ]
+    }
+)
---
+++ snoonet/auth.py
@@ -0,0 +1,6 @@
+class auth_bot:
+    def __init__(self):
+        print "started the auth bot"
+
+def start_auth_bot():
+    authbot = auth_bot()
