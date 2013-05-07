7a4681d18c129060181f76f2b7e2ac39b25caf0b
t0mcat <diminoten@snoonet.org>
2013-05-07 23:18:45 +0000

    Fixed #10, #5, #6, #2

--- snoonet/auth.py
+++ snoonet/auth.py
@@ -4,6 +4,12 @@ from twisted.python import log
 import xmlrpclib as xmlrpc
 import sys
 
+class InvalidStatusCodeException(Exception):
+    def __init__(self, message):
+        self.message = message 
+    def __str__(self):
+        return repr(self.message)
+
 class AuthBot:
     got_Pong = True
 
@@ -14,76 +20,103 @@ class AuthBot:
         self.channels = config.get('auth_bot', 'channels').split(', ')
 
     def xmlrpc_auth(self):
-        self.server = xmlrpc.Server(self.server_url)
-        result = self.server.atheme.login(self.nick, self.passwd)
-        self.authcookie = result.authcookie
+        try:
+            self.server = xmlrpc.Server(self.server_url)
+            result = self.server.atheme.login(self.nick, self.passwd)
+        
+            self.authcookie = result.authcookie
+        except xmlrpx.Fault as fault:
+            raise fault
+
+    def xmlrpc_send_command(service_name, command_name, *parameters):
+        try:
+            result = self.server.atheme.command(self.authcookie, self.user, self.source_ip, service_name, command_name, *parameters)
+        except xmlrpc.Fault as fault:
+            if fault.faultCode is 4:
+                return False
+            elif fault.faultCode is 15:
+                self.xmlrpc_auth()
+                self.xmlrpc_send_command(service_name, command_name, *parameters)
+            elif fault.faultCode is 12:
+                pass
+            else:
+                raise fault
+        
+        return True
 
     def is_key_valid(self, key):
         allowed_set = set(string.uppercase + string.lowercase + string.digits)
         return all(x in allowed_set for x in key) and len(key) is 25
 
     def is_user_registered(self, username):
-        try:
-            result = self.server.atheme.command(self.authcookie, self.user, self.source_ip, 'nickserv', 'info', username)
-
-        except xmlrpclib.Fault as fault:
-            if fault.faultCode is 4:
-                return False
-            else:
-                raise fault
-
-        return True
+        return self.xmlrpc_send_command('nickserv', 'info', username)
 
     def is_channel_registered(self, channel):
-        try:
-            result = self.server.atheme.command(self.authcookie, self.user, self.source_ip, 'chanserv', 'info', channel)
-        except xmlrpclib.Fault as fault:
-            if fault.faultCode is 4:
-                return False
-            else:
-                raise fault
-
-        return True
+        return self.xmlrpc_send_command('chanserv', 'info', channel)
 
     def set_user_channel_modes(self, user, channel, modes):
-        try:
-            self.server.atheme.command(self.authcookie, self.user, self.source_ip, 'chanserv', 'flags', channel, '+%s' % modes)
-        except xmlrpclib.Fault as fault:
-            if fault.faultCode is 12:
-                return True
-            else:
-                raise fault
-
-        return True
+        return self.xmlrpc_send_command('chanserv', 'flags', channel, '+%s' % modes)
 
     def create_channel(self, channel, user):
         self.join(channel)
         self.say('ChanServ', 'REGISTER %s' %channel)
         self.server.atheme.command(self.authcookie, self.nick,'127.0.0.1','ChanServ', 'ftransfer', channel, user)
         self.leave(channel)
+    
+    def validate_key(self, key):
+        return self._rest_communicate('/key', key)        
+
+    def expire_key(self, key):
+        return self._rest_communicate('/use', key, {'status':'true'})
+
+    def _rest_communicate(self, path, key, data=None):
+        url = self.api_url + path
+        try:    
+            response = requests.get(url, params={'key':key}, data=data)
+        except requests.RequestException as request_exception:
+            log.err('Requests exception! key=%s, url=%s' %(key, url))
+            raise request_exception
+    
+        if response.status_code is not 200:
+            log.err('Response from Snoonet REST API: %s' % response.status_code)
+            raise InvalidStatusCodeException('Invalid response code from REST API [%s].' % response.status_code)
+
+        return response.json()
 
     def signedOn(self):
         for chan in self.channels:
-            log.msg('Joining channel ' + chan)
+            self.join(chan)
 
         xmlrpc_auth()
 
-    def privmsg(self, user, channel, msg):
-        if channel == self.nick:
-            log.msg('Msg received from %s' % user)
-            split_message = msg.split()
+    def process_auth(self, user, channel, key):
+        data = self.validate_key(key)
 
-            if len(split_message) != 2:
-                self.say(user, "Invalid argument count.")
-                return
+        subreddit_channel = '#' + data['channel']
+        subreddit_name = data['subreddit']
+        channel_mode = data['mode']
 
-            action = split_message[0]
-            key = split_message[1]
+        if not self.is_channel_registered(subreddit_channel):
+            self.create_channel(subreddit_channel, user, message="Registered channel for the %s subreddit" % subreddit_name)
+        else:
+            self.set_user_channel_modes(user, subreddit_channel, channel_mode)
 
-            if action is not "subreddit_access":
-                self.say(user, "Invalid action in first argument.")
-                return
+        self.expire_key(key)
 
+    def process_whisper(self, user, msg):
+        split_message = msg.split()
+
+        if len(split_message) != 2:
+            self.say(user, "Invalid argument count.")
+            return
+
+        action = split_message[0]
+        key = split_message[1]
+
+        if action is not "subreddit_access":
+            self.say(user, "Invalid action in first argument.")
+            return
+        try:
             if not self.is_key_valid(key):
                 self.say(user, "Key not properly formed.")
                 return
@@ -92,32 +125,21 @@ class AuthBot:
                 self.say(user, "You are not registered.")
                 return
 
-            url = 'http://api.snoonet.org/api/v1/modekey/key'
-            response = requests.get(url, params={'key':key})
+            self.process_auth(user, channel, key)
 
-            if response.status_code is not 200:
-                self.say(user, "Error getting response back from auth server.")
-                log.err('non-200 response received from auth server.  Actual status code was %s' % response.status_code)
-                return
+        except InvalidStatusCodeException, requests.RequestException) as api_exception:
+            self.say(user, "Error communicating with Snoonet REST API.")
+            raise api_exception
+        except xmlrpc.Fault as fault:
+            self.say(user, "Error communicating with Atheme services.")
+            raise fault
+        except:
+            self.say(user, "Unknown error.")
+            raise
 
-            data = response.json()
-            subreddit_channel = '#' + data['channel']
-            subreddit_name = data['subreddit']
-            channel_mode = data['mode']
-
-            if not self.is_channel_registered(subreddit_channel):
-                self.create_channel(subreddit_channel, user, message="Registered channel for the %s subreddit" % subreddit_name)
-            else:
-                self.set_user_channel_modes(user, subreddit_channel, channel_mode)
-
-            post_url = 'http://api.snoonet.org/api/v1/modekey/use'
-            post_data = {'status':'true'}
-            response = requests.post(post_url, data=post_data, params={'use':key})
-
-            if response.status_code is not 200:
-                log.err('non-200 response received from auth server.  Actual status code was %s' % response.status_code)
-
-            self.say(user, "Success! :)")
+    def privmsg(self, user, channel, msg):
+        if channel == self.nick:
+            self.process_whisper(user, msg)
 
 class AuthBotFactory(protocol.ClientFactory):
     def __init__(self, config):
